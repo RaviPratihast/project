f2425c808eec1d0a7df73e0b297eb349b365a35a
Ravi Pratihast <ravipratihast@gmail.com>
2024-12-25 16:17:52 +0530

    Refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-//   darkMode: 'class',
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
-// tailwind.config.js
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // Adjust based on your file structure
